1df411a1ad97a13487f0bfb5f10a823cbf1c95a2
Ingrycht Alves Antunes <ingrycht@gmail.com>
2018-02-02 14:39:16 +0000

    add take a picture feature to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { TabsPage } from '../pages/tabs/tabs';
+import {Camera} from '@ionic-native/camera';
+
 
 @Component({
   templateUrl: 'app.html'
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public photos: any;
+  public base64Image: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera, private alertCtrl: AlertController) {
 
   }
+  ngOnInit() {
+    this.photos = [];
+  }
 
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.photos.push(this.base64Image);
+      this.photos.reverse();
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  deletePhoto(index) {
+    let confirm = this.alertCtrl.create({
+      title: 'ure you want to delete this picture?',
+      message: '',
+      buttons: [
+        {
+          text: 'NO',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.photos.splice(index, 1);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 }
